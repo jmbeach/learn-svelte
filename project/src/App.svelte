6dde3375bae1252a775456781b565d5e68aaf568
jmbeach <jaredbeachdesign@gmail.com>
2023-01-15 10:06:58 -0500

    save

--- project/src/App.svelte
+++ project/src/App.svelte
@@ -1,9 +1,27 @@
 <script>
-
 </script>
 
+<header>
+  <h1>MeetUs</h1>
+</header>
+
 <style>
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 4rem;
+    background: #e11d48;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+  }
 
+  h1 {
+    color: white;
+    font-family: 'Roboto Slab', sans-serif;
+    margin: 0;
+  }
 </style>
-
-<h1>Time for a project!</h1>
