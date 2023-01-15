4bdc4b9ec90715b0f5712de1262b601119345d30
jmbeach <jaredbeachdesign@gmail.com>
2023-01-15 16:04:43 -0500

    more scaffold

--- project/src/App.svelte
+++ project/src/App.svelte
@@ -1,27 +1,28 @@
 <script>
+  import Header from './UI/Header.svelte';
+  const meetups = [
+    {
+      id: 'm1',
+      title: 'Coding Bootcamp',
+      subtitle: 'Learn to code in 2 hours',
+      description:
+        'In this meetup, we will have some experts that teach you how to code',
+      imgUrl:
+        'https://images.unsplash.com/photo-1531498860502-7c67cf02f657?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+      address: '27th Nerd Road, 32523 New York',
+      contactEmail: 'code@test.com',
+    },
+    {
+      id: 'm2',
+      title: 'Swim Together',
+      subtitle: "Let's go swimming!",
+      description: 'We will simply swim some rounds!',
+      imgUrl:
+        'https://images.unsplash.com/photo-1589584556363-b90f6bcc7149?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+      address: '27th Nerd Road, 32523 New York',
+      contactEmail: 'swim@test.com',
+    },
+  ];
 </script>
 
-<header>
-  <h1>MeetUs</h1>
-</header>
-
-<style>
-  header {
-    position: fixed;
-    width: 100%;
-    top: 0;
-    left: 0;
-    height: 4rem;
-    background: #e11d48;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
-  }
-
-  h1 {
-    color: white;
-    font-family: 'Roboto Slab', sans-serif;
-    margin: 0;
-  }
-</style>
+<Header />
---
+++ project/src/UI/Header.svelte
@@ -0,0 +1,23 @@
+<header>
+  <h1>MeetUs</h1>
+</header>
+
+<style>
+  h1 {
+    color: white;
+    font-family: 'Roboto Slab', sans-serif;
+    margin: 0;
+  }
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 4rem;
+    background: #e11d48;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+  }
+</style>
