ee5606607f6cceee25a2016e27681ab728030ad1
jmbeach <jaredbeachdesign@gmail.com>
2023-01-16 09:40:21 -0500

    add meetup

--- project/src/App.svelte
+++ project/src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import Header from './UI/Header.svelte';
   import MeetupGrid from './Meetups/MeetupGrid.svelte';
-  const meetups = [
+  let meetups = [
     {
       id: 'm1',
       title: 'Coding Bootcamp',
@@ -24,11 +24,60 @@
       contactEmail: 'swim@test.com',
     },
   ];
+  function addMeetup() {
+    meetups = [
+      {
+        id: new Date().getTime(),
+        title,
+        subtitle,
+        imgUrl,
+        description,
+        address,
+        contactEmail,
+      },
+      ...meetups,
+    ];
+  }
+  export let title;
+  export let subtitle;
+  export let imgUrl;
+  export let description;
+  export let address;
+  export let contactEmail;
 </script>
 
 <Header />
 
-<main><MeetupGrid {meetups} /></main>
+<main>
+  <form on:submit|preventDefault={addMeetup}>
+    <div class="form-control">
+      <label for="title">Title</label>
+      <input type="text" id="title" bind:value={title} />
+    </div>
+    <div class="form-control">
+      <label for="subtitle">Subtitle</label>
+      <input type="text" id="subtitle" bind:value={subtitle} />
+    </div>
+    <div class="form-control">
+      <label for="address">Address</label>
+      <input type="text" id="address" bind:value={address} />
+    </div>
+    <div class="form-control">
+      <label for="imgUrl">Image URL</label>
+      <input type="text" id="imgUrl" bind:value={imgUrl} />
+    </div>
+    <div class="form-control">
+      <label for="email">Email</label>
+      <input type="email" id="email" bind:value={contactEmail} />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea rows="3" id="description" bind:value={description} />
+    </div>
+    <button type="submit">Save</button>
+  </form>
+  <MeetupGrid {meetups} />
+</main>
 
 <style>
   main {
