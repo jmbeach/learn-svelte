0d0d17c3b643e1cc40d5565925a8e2351b758895
jmbeach <jaredbeachdesign@gmail.com>
2023-01-21 17:51:46 -0500

    tick example

--- 02/src/App.svelte
+++ 02/src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Modal from './Modal.svelte';
   import Product from './Product.svelte';
+  import { tick, afterUpdate } from 'svelte';
 
   const products = [
     {
@@ -10,12 +11,29 @@
     },
   ];
   let showModal = false;
+  let text = 'This is some dummy text';
 
   function addToCart(e) {
     console.log(e.detail.id);
   }
   function onClose() {
     showModal = false;
+  }
+
+  function transform(e) {
+    if (e.which !== 9) return;
+    e.preventDefault();
+    const {
+      target: { value, selectionStart, selectionEnd },
+    } = e;
+    text =
+      value.slice(0, selectionStart) +
+      value.slice(selectionStart, selectionEnd).toUpperCase() +
+      value.slice(selectionEnd);
+    tick().then(() => {
+      e.target.selectionStart = selectionStart;
+      e.target.selectionEnd = selectionEnd;
+    });
   }
 </script>
 
@@ -38,3 +56,5 @@
     >
   </Modal>
 {/if}
+
+<textarea rows="5" on:keydown={transform}>{text}</textarea>
