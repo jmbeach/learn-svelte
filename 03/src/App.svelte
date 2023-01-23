3319040e137a2243bcda1142d9aa3f77da8f4976
jmbeach <jaredbeachdesign@gmail.com>
2023-01-23 07:54:18 -0500

    groups

--- 03/src/App.svelte
+++ 03/src/App.svelte
@@ -4,12 +4,24 @@
   let val = 'Jared';
   let selectedOption = 1;
   let price = 0;
-  $: console.log(price);
-  function setValue(e) {
-    val = e.target.value;
-  }
+  let agreed;
+  let favColor = ['green'];
+  $: console.log(favColor);
 </script>
 
 <CustomInput bind:val type="text" />
 <Toggle bind:chosenOption={selectedOption} />
 <input type="number" bind:value={price} />
+<label><input type="checkbox" bind:checked={agreed} />Agree to terms?</label>
+<label>
+  <input type="checkbox" name="color" value="red" bind:group={favColor} />
+  Red
+</label>
+<label>
+  <input type="checkbox" name="color" value="green" bind:group={favColor} />
+  Green
+</label>
+<label>
+  <input type="checkbox" name="color" value="blue" bind:group={favColor} />
+  Blue
+</label>
