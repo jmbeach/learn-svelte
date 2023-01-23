b03cf336c39b1e7cced8811a70774a18c6cf2e75
jmbeach <jaredbeachdesign@gmail.com>
2023-01-23 08:16:47 -0500

    validation

--- 03/src/App.svelte
+++ 03/src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import CustomInput from './CustomInput.svelte';
   import Toggle from './Toggle.svelte';
+  import { isValidEmail } from './validation';
   let val = 'Jared';
   let selectedOption = 1;
   let price = 0;
@@ -10,6 +11,13 @@
   let usernameInput;
   let customInput;
   let someDiv;
+  let enteredEmail = '';
+  let formIsValid = false;
+  $: if (isValidEmail(enteredEmail)) {
+    formIsValid = true;
+  } else {
+    formIsValid = false;
+  }
   function saveData() {
     console.log(someDiv);
     customInput.empty();
@@ -46,3 +54,20 @@
 <button on:click={saveData}>Save</button>
 
 <div bind:this={someDiv} />
+
+<hr />
+
+<form on:submit|preventDefault>
+  <input
+    type="email"
+    bind:value={enteredEmail}
+    class={isValidEmail(enteredEmail) ? '' : 'invalid'}
+  />
+  <button disabled={!formIsValid}>Save</button>
+</form>
+
+<style>
+  .invalid {
+    border: 1px solid red;
+  }
+</style>
