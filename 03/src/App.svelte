144fc8a9f4e5e9747e5f052484cee9b717b66010
jmbeach <jaredbeachdesign@gmail.com>
2023-01-23 07:49:45 -0500

    save

--- 03/src/App.svelte
+++ 03/src/App.svelte
@@ -1,5 +1,15 @@
 <script>
-
+  import CustomInput from './CustomInput.svelte';
+  import Toggle from './Toggle.svelte';
+  let val = 'Jared';
+  let selectedOption = 1;
+  let price = 0;
+  $: console.log(price);
+  function setValue(e) {
+    val = e.target.value;
+  }
 </script>
 
-<h1>Bindings & Forms</h1>
+<CustomInput bind:val type="text" />
+<Toggle bind:chosenOption={selectedOption} />
+<input type="number" bind:value={price} />
---
+++ 03/src/CustomInput.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let val;
+</script>
+
+<input type="text" bind:value={val} />
