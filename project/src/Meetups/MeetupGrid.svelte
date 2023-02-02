d62d040d42043ec8b054c03624941ee9b4a0eac4
jared.beach <jared.beach@capitalone.com>
2023-02-01 19:56:59 -0500

    filter buttons

--- project/src/Meetups/MeetupGrid.svelte
+++ project/src/Meetups/MeetupGrid.svelte
@@ -1,24 +1,39 @@
 <script>
   import MeetupItem from "./MeetupItem.svelte";
+  import MeetupFilter from "./MeetupFilter.svelte";
   export let meetups;
+  $: filtered = favsOnly ? meetups.filter((x) => x.isFavorite) : meetups;
+  let favsOnly = false;
+
+  function setFilter(e) {
+    favsOnly = e.detail === 1;
+  }
 </script>
 
+<section id="meetup-controls">
+  <MeetupFilter on:select={setFilter} {favsOnly} />
+</section>
+
 <section id="meetups">
-  {#each meetups as meetup (meetup.id)}
+  {#each filtered as meetup (meetup.id)}
     <MeetupItem {...meetup} on:showdetails on:edit />
   {/each}
 </section>
 
 <style>
-  section {
+  #meetups {
     width: 100%;
     display: grid;
     grid-template-columns: 1fr;
     grid-gap: 1rem;
   }
 
+  #meetup-controls {
+    margin: 1rem;
+  }
+
   @media (min-width: 768px) {
-    section {
+    #meetups {
       grid-template-columns: repeat(2, 1fr);
     }
   }
