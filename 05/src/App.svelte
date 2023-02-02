8dc81ea80901a998ed88800d1498eb9194cb8223
jared.beach <jared.beach@capitalone.com>
2023-02-01 20:47:54 -0500

    spring

--- 05/src/App.svelte
+++ 05/src/App.svelte
@@ -2,6 +2,7 @@
   import { writable } from "svelte/store";
   import { tweened } from "svelte/motion";
   import { cubicIn } from "svelte/easing";
+  import Spring from "./Spring.svelte";
 
   const progress = tweened(0, {
     delay: 0,
@@ -14,4 +15,5 @@
   }, 1500);
 </script>
 
-<progress value={$progress} />
+<!-- <progress value={$progress} /> -->
+<Spring />
--- 05/src/Spring.svelte
+++ 05/src/Spring.svelte
@@ -1,45 +1,52 @@
 <script>
   import { writable } from "svelte/store";
-  
+  import { spring } from "svelte/motion";
+
   let cards = writable([
     {
       id: "c1",
-      color: "red"
+      color: "red",
     },
     {
       id: "c2",
-      color: "blue"
+      color: "blue",
     },
     {
       id: "c3",
-      color: "green"
+      color: "green",
     },
     {
       id: "c4",
-      color: "orange"
-    }
+      color: "orange",
+    },
   ]);
-  let cardPos = writable([
+  let cardPos = spring(
+    [
+      {
+        rotation: 10,
+        dx: 0,
+      },
+      {
+        rotation: -10,
+        dx: 0,
+      },
+      {
+        rotation: 19,
+        dx: 0,
+      },
+      {
+        rotation: -25,
+        dx: 0,
+      },
+    ],
     {
-      rotation: 10,
-      dx: 0
-    },
-    {
-      rotation: -10,
-      dx: 0
-    },
-    {
-      rotation: 19,
-      dx: 0
-    },
-    {
-      rotation: -25,
-      dx: 0
+      stiffness: 0.05,
+      damping: 0.9,
     }
-  ]);
+  );
 
   function discard(index) {
-    cardPos.update(items => {
+    cardPos.update((items) => {
       const updatedCardPositions = [...items];
       const updatedCardPos = { ...updatedCardPositions[index] };
       updatedCardPos.dx = 1200;
@@ -49,6 +56,19 @@
     });
   }
 </script>
+
+<div class="page">
+  <div class="cards">
+    {#each $cards as card, i (card.id)}
+      <div
+        class="card"
+        style="background: {card.color}; transform: rotateZ({$cardPos[i]
+          .rotation}deg) translateX({$cardPos[i].dx}px)"
+        on:click={discard.bind(this, i)}
+      />
+    {/each}
+  </div>
+</div>
 
 <style>
   .page {
@@ -70,14 +90,3 @@
     border-radius: 5px;
   }
 </style>
-
-<div class="page">
-  <div class="cards">
-    {#each $cards as card, i (card.id)}
-      <div
-        class="card"
-        style="background: {card.color}; transform: rotateZ({$cardPos[i].rotation}deg) translateX({$cardPos[i].dx}px)"
-        on:click={discard.bind(this, i)} />
-    {/each}
-  </div>
-</div>
