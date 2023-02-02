df33f52c1e4c3fcf331053e437d7bf94c188ab77
jared.beach <jared.beach@capitalone.com>
2023-02-01 20:42:01 -0500

    tweened

--- 05/src/App.svelte
+++ 05/src/App.svelte
@@ -1,5 +1,17 @@
 <script>
+  import { writable } from "svelte/store";
+  import { tweened } from "svelte/motion";
+  import { cubicIn } from "svelte/easing";
+
+  const progress = tweened(0, {
+    delay: 0,
+    duration: 700,
+    easing: cubicIn,
+  });
+
+  setTimeout(() => {
+    progress.set(0.5);
+  }, 1500);
 </script>
 
-
-
+<progress value={$progress} />
