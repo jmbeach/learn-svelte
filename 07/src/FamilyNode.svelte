7fde8c0e910bc50ba5d926129d5b4f51edae05a8
jmbeach <jaredbeachdesign@gmail.com>
2023-02-12 13:46:15 -0500

    run once per type

--- 07/src/FamilyNode.svelte
+++ 07/src/FamilyNode.svelte
@@ -1,13 +1,36 @@
-<script>
-  export let member;
+<script context="module">
+  console.log("hello world");
+  let deactivate;
 </script>
 
-<h1>{member.name}</h1>
-{#if member.isParent && member.children?.length}
-  <ul>
-    {#each member.children as child}
-      <svelte:self member={child} />
-    {/each}
-  </ul>
-{/if}
-<hr />
+<script>
+  export let member;
+  let isActive = false;
+  console.log("every time");
+  function activate() {
+    if (deactivate) deactivate();
+    isActive = true;
+    deactivate = deactivateNode;
+  }
+  function deactivateNode() {
+    isActive = false;
+  }
+</script>
+
+<div on:click={activate} class:active={isActive}>
+  <h1>{member.name}</h1>
+  {#if member.isParent && member.children?.length}
+    <ul>
+      {#each member.children as child}
+        <svelte:self member={child} />
+      {/each}
+    </ul>
+  {/if}
+  <hr />
+</div>
+
+<style>
+  .active {
+    color: red;
+  }
+</style>
