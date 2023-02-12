d2bec4170ed2a2bbe1fa6d644f25f75863d3c3c5
jmbeach <jaredbeachdesign@gmail.com>
2023-02-12 13:17:59 -0500

    recursive

--- 07/src/App.svelte
+++ 07/src/App.svelte
@@ -1,7 +1,22 @@
 <script>
   import Product from "./Product.svelte";
   import CartItem from "./CartItem.svelte";
+  import FamilyNode from "./FamilyNode.svelte";
   let showProducts = true;
+  let familyStructure = [
+    {
+      isParent: true,
+      name: "Chris",
+      children: [
+        {
+          isParent: true,
+          name: "Moe",
+          children: [{ isParent: false, name: "Julie" }],
+        },
+      ],
+    },
+    { isParent: true, name: "Anna" },
+  ];
   function toggle() {
     showProducts = !showProducts;
   }
@@ -13,3 +28,7 @@
   title={showProducts ? "test product" : "test cart item"}
   id="p1"
 />
+
+{#each familyStructure as familyMember}
+  <FamilyNode member={familyMember} />
+{/each}
---
+++ 07/src/FamilyNode.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let member;
+</script>
+
+<h1>{member.name}</h1>
+{#if member.isParent && member.children?.length}
+  <ul>
+    {#each member.children as child}
+      <svelte:self member={child} />
+    {/each}
+  </ul>
+{/if}
+<hr />
