01d9c49c59a70ff7e5d53499a07384fc8bd0dbc6
jmbeach <jaredbeachdesign@gmail.com>
2023-02-12 13:09:09 -0500

    dynamic components

---
+++ 07/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Product from "./Product.svelte";
+  import CartItem from "./CartItem.svelte";
+  let showProducts = true;
+  function toggle() {
+    showProducts = !showProducts;
+  }
+</script>
+
+<button on:click={toggle}>Toggle Display</button>
+<svelte:component
+  this={showProducts ? Product : CartItem}
+  title={showProducts ? "test product" : "test cart item"}
+  id="p1"
+/>
